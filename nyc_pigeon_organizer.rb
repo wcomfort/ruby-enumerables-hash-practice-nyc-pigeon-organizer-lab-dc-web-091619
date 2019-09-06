f47142285e2c085533ad4e47bd51775005123a4c
Will Comfort <comfortwb21@gmail.com>
2019-09-06 22:25:51 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  bird_names = {}
+  data.each do |color_gender_lives, value|
+    value.each do |stats, names|
+      names.each do |pigeon_names|
+        if bird_names[pigeon_names] == nil 
+          bird_names[pigeon_names] = {}
+        end
+        if bird_names[pigeon_names][color_gender_lives] == nil 
+           bird_names[pigeon_names][color_gender_lives] = []
+         end
+         bird_names[pigeon_names][color_gender_lives].push(stats.to_s)
+       end
+     end
+   end 
+      bird_names
 end
