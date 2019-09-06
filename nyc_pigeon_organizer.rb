d9c4dffb61c7d69025b9004837343d801fc3b397
Will Comfort <comfortwb21@gmail.com>
2019-09-06 22:23:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,7 @@ def nyc_pigeon_organizer(data)
   data.each do |color_gender_lives, value|
     value.each do |stats, names|
       names.each do |pigeon_names|
-        if bird_names[pigeon_names] =
+        if bird_names[pigeon_names] == nil 
+          bird_names = {}
       
 end
